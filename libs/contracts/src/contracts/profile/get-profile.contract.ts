a0e082d331dd45b3770ee8f2e763ce68e30b9824
Max Bezditko <mb.preply.1@gmail.com>
2024-10-30 07:30:33 +0200

    update users
    update docker compose configuration
    
    implement tasks-developers relation
    
    refactor tasks_developers to tasks_users
    
    implement assign task endpoint
    
    implement role based auth guards
    
    implement unassign task endpoint
    
    implement actions
    
    change comment entity
    
    implement create comment
    
    implement get comments endpoint
    
    refactor queue names
    
    refactor contract names 2
    
    implement task participants joins

--- libs/contracts/src/contracts/profile/get-profile.contract.ts
+++ libs/contracts/src/contracts/profile/get-profile.contract.ts
@@ -1,16 +1,16 @@
 import { Errorable } from '@taskfusion-microservices/types';
 import {
   GENERAL_EXCHANGE_NAME,
-  USERS_QUEUE_NAME,
+  PROFILES_QUEUE_NAME,
 } from '@taskfusion-microservices/constants';
 import { UserEntity } from '@taskfusion-microservices/entities';
 
 export namespace GetProfileContract {
   export const exchange = GENERAL_EXCHANGE_NAME;
 
-  export const routingKey = `get-client`;
+  export const routingKey = `get-profile`;
 
-  export const queue = `${USERS_QUEUE_NAME}.${routingKey}`;
+  export const queue = `${PROFILES_QUEUE_NAME}.${routingKey}`;
 
   export type Response = Errorable<
     Pick<
