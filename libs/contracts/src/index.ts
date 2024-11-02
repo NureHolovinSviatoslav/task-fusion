274b99fba49151abdd5f0e011699bea67bd59262
Max Bezditko <mb.preply.1@gmail.com>
2024-11-02 09:47:27 +0200

    add actions for task participant endpoints
    add projects users db
    
    add projects users entity
    
    add projects users microservice
    
    implement create invite for pm endpoint
    
    update contracts for invite endpoints
    
    implement endpoint to accept invites
    
    implement reject invite endpoint
    
    add projects users contracts
    
    add project pm user handler
    
    implement check pm email endpoint
    
    implement email module
    
    implement get user rpcs
    
    refactor create pm invite endpoint

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -7,14 +7,22 @@ export * from './contracts/developers/check-developer.contract';
 
 export * from './contracts/pms/create-pm.contract';
 export * from './contracts/pms/check-pm.contract';
+export * from './contracts/pms/check-pm-email.contract';
 
 export * from './contracts/users/check-user.contract';
 export * from './contracts/users/get-users-by-ids.contract';
+export * from './contracts/users/get-user-by-id.contract';
+export * from './contracts/users/get-user-by-email.contract';
 
 export * from './contracts/projects/create-project.contract';
 export * from './contracts/projects/check-project.contract';
 export * from './contracts/projects/get-projects.contract';
 export * from './contracts/projects/get-project-by-id.contract';
+export * from './contracts/projects/get-project-pm-user.contract';
+
+export * from './contracts/projects-users/get-project-pm-id.contract';
+export * from './contracts/projects-users/assign-user-to-project.contract';
+export * from './contracts/projects-users/unassign-user-from-project.contract';
 
 export * from './contracts/auth/refresh-tokens.contract';
 export * from './contracts/auth/logout.contract';
@@ -25,15 +33,21 @@ export * from './contracts/profile/get-profile.contract';
 export * from './contracts/tasks/create-task.contract';
 export * from './contracts/tasks/get-tasks-by-status.contract';
 export * from './contracts/tasks/get-task-by-id.contract';
-export * from './contracts/tasks/assign-task-to-user.contract';
-export * from './contracts/tasks/unassign-task-from-user.contract';
 export * from './contracts/tasks/check-task.contract';
 export * from './contracts/tasks/change-task-status.contract';
 
 export * from './contracts/tasks-users/get-task-participants.contract';
+export * from './contracts/tasks-users/assign-task-to-user.contract';
+export * from './contracts/tasks-users/unassign-task-from-user.contract';
 
 export * from './contracts/actions/create-action.contract';
 export * from './contracts/actions/get-actions-by-task-id.contract';
 
 export * from './contracts/comments/create-comment.contract';
 export * from './contracts/comments/get-comments-by-task-id.contract';
+
+export * from './contracts/invites/invite-pm.contract';
+export * from './contracts/invites/accept-pm-invite.contract';
+export * from './contracts/invites/reject-pm-invite.contract';
+
+export * from './contracts/email/send-email.contract';
---
+++ libs/entities/src/lib/invite.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
+import { ProjectEntity } from './project.entity';
+
+export enum InviteStatus {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+}
+
+@Entity({
+  name: 'invites',
+})
+export class InviteEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  projectId: number;
+
+  @Column()
+  clientUserId: number;
+
+  @Column()
+  pmUserId: number;
+
+  @Column()
+  expiresAt: Date;
+
+  @Column({
+    type: 'enum',
+    enum: InviteStatus,
+    name: 'invite_status',
+  })
+  inviteStatus: InviteStatus;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => ProjectEntity, (project) => project.invites)
+  project: ProjectEntity;
+}
